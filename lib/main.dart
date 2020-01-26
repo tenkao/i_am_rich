03616eb9471903c4502b7fb807adaefbedd10a8f
Hagiwara Tsutomu <tenkao@gmail.com>
2020-01-27 00:35:43 +0900

    27. Scaffolding a Flutter App
    - Scaffold() - マテリアルデザインのスキャッフォールディング
    - backgroundColor: - 背景色
    - Colors.*** - マテリアルデザインのカラーコードを使用できる
    - appBar: AppBar() - アプリのヘッダー
    - body: - アプリのメイン部分
    - Image() - 画像
    - image: NetworkImage('URL') - URL の画像を表示する
    - Option + Enter - クイック修正案を表示（Wrap with Center とか）
    - Preferences > ... > Show the documentation popup - コード補完候補にクイックドキュメントを表示する

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all our Flutter apps.
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://www.w3schools.com/w3css/img_lights.jpg'),
+            ),
+          ),
         ),
       ),
     );
